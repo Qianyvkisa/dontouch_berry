2d1b1734bf3e11bc70e79eccba63542396419d86
llama <junyu_eden@qq.com>
2023-04-30 21:38:12 +0800

    2023.4.30

--- src/main/java/io/github/Qianyvkisa/don_touch_berry/DonTouchBerry.java
+++ src/main/java/io/github/Qianyvkisa/don_touch_berry/DonTouchBerry.java
@@ -1,12 +1,15 @@
 package io.github.Qianyvkisa.don_touch_berry;
 
+import io.github.Qianyvkisa.don_touch_berry.blocks.the_god_berry_bush;
 import io.github.Qianyvkisa.don_touch_berry.items.the_god_berry;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -21,6 +24,8 @@ public class DonTouchBerry implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 
+
+
 	public static final String ModID = "don_touch_berry";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ModID);
 
@@ -29,8 +34,9 @@ public class DonTouchBerry implements ModInitializer {
 					new the_god_berry(new FabricItemSettings()));
 
 
-	public static final Block the_god_berry_bush = new PlantBlock(FabricBlockSettings.of(Material.BARRIER).breakInstantly().strength(0.0f,0.0f).noCollision().sounds(BlockSoundGroup.SWEET_BERRY_BUSH));
+	public static final Block the_god_berry_bush = new the_god_berry_bush(FabricBlockSettings.of(Material.BARRIER).breakInstantly().strength(0.0f,0.0f).noCollision().sounds(BlockSoundGroup.SWEET_BERRY_BUSH).nonOpaque());
 	public static final BlockItem the_god_berry_bush_block = new BlockItem(the_god_berry_bush,new FabricItemSettings());
+
 
 	private static final ItemGroup The_Berry = FabricItemGroup.builder(new Identifier(ModID, "the_berry"))
 			.icon(() -> new ItemStack(the_god_berry))
@@ -43,14 +49,12 @@ public class DonTouchBerry implements ModInitializer {
 		// Proceed with mild caution.
 
 
-		Registry.register(Registries.BLOCK, new Identifier(ModID,"the_god_berry_bush"),the_god_berry_bush);
-		Registry.register(Registries.ITEM, new Identifier(ModID,"the_god_berry_bush_block"),the_god_berry_bush_block);
-
+		Registry.register(Registries.BLOCK, new Identifier(ModID, "the_god_berry_bush"), the_god_berry_bush);
+		Registry.register(Registries.ITEM, new Identifier(ModID, "the_god_berry_bush_block"), the_god_berry_bush_block);
 		ItemGroupEvents.modifyEntriesEvent(The_Berry).register(content -> {
 			content.add(the_god_berry_bush_block);
 		});
 
-
-
+		BlockRenderLayerMap.INSTANCE.putBlock(the_god_berry_bush, RenderLayer.getCutout());
 	}
 }
---
+++ src/main/java/io/github/Qianyvkisa/don_touch_berry/blocks/the_god_berry_bush.java
@@ -0,0 +1,11 @@
+package io.github.Qianyvkisa.don_touch_berry.blocks;
+
+import net.minecraft.block.*;
+
+
+public class the_god_berry_bush extends PlantBlock {
+
+    public the_god_berry_bush(Settings settings) {
+        super(settings);
+    }
+}
